087907bb86d2044c3b1a278783ec1759b42771f3
Valentine Zaremsky <zaremsky@gmail.com>
2021-05-17 19:41:29 +0300

    try green color theme

--- src/utils/vars.js
+++ src/utils/vars.js
@@ -1,4 +1,17 @@
 export const colors = {
+  main: `#008800`,
+  second: `#eeffee`,
+  link: `#00aa00`,
+  light: `#aaddaa`,
+  main50: `rgba(0, 136, 0, 0.5)`,
+  second50: `rgba(238, 256, 238, 0.5)`,
+  textMain: `#004400`,
+  textSecond: `#ffffff`,
+  textBody: `#337733`
+};
+
+/**
+ *
   main: `#880000`,
   second: `#ffeeee`,
   link: `#aa0000`,
@@ -8,4 +21,4 @@ export const colors = {
   textMain: `#440000`,
   textSecond: `#ffffff`,
   textBody: `#773333`
-};
+ */
